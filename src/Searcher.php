fcbe2ceed778e6602bce7b5dab1fbe7c5a4260dd
Santiago <victor.lacsilva0102@gmail.com>
2023-01-18 16:04:35 -0300

    refactor: refactoring under PSR12 rules

--- src/Searcher.php
+++ src/Searcher.php
@@ -7,28 +7,28 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Searcher
 {
-  private $httpClient;
-  private $crawler;
+    private $httpClient;
+    private $crawler;
 
-  public function __construct(ClientInterface $httpClient, Crawler $crawler)
-  {
-    $this->httpClient = $httpClient;
-    $this->crawler = $crawler;
-  }
-
-  public function search(string $url): array
-  {
-    $response = $this->httpClient->request('GET', $url);
-    $html = $response->getBody();
-
-    $this->crawler->addHtmlContent($html);
-    $courseElements = $this->crawler->filter('span.card-curso__nome');
-    $courses = [];
-
-    foreach ($courseElements as $element) {
-      $courses[] = $element->textContent;
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
     }
 
-    return $courses;
-  }
+    public function search(string $url): array
+    {
+        $response = $this->httpClient->request('GET', $url);
+        $html = $response->getBody();
+
+        $this->crawler->addHtmlContent($html);
+        $courseElements = $this->crawler->filter('span.card-curso__nome');
+        $courses = [];
+
+        foreach ($courseElements as $element) {
+            $courses[] = $element->textContent;
+        }
+
+        return $courses;
+    }
 }
